49a73545daaa70ee6e11d61908d71d201e4f5986
Fedor Kuzminov <riyce@mail.ru>
2025-03-19 23:45:13 +0100

    migration 3 fix

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -4,7 +4,7 @@ INSERT INTO product (id, name, picture_url, price) VALUES
 (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
 (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
 (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
-(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00)
+(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
 
 INSERT INTO orders (id, status, date_created)
